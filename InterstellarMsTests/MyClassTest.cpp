e99e951f383ec69b4f6d41fc0d6f278205ba8c2b
Vasco <vhorta@gmail.com>
2025-07-18 23:49:55 +0100

    Adds MS Unit Testing.

---
+++ InterstellarMsTests/MyClassTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"                // precompiled header, includes TestTools
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace InterstellarMsTests
+{
+    TEST_CLASS(MyClassTests)
+    {
+    public:
+        TEST_METHOD(TestSeed)
+        {
+            Assert::AreEqual(7, 7);
+            Assert::AreNotEqual(7, 8);
+        }
+    };
+}
