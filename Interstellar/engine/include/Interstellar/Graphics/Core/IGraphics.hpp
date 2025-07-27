2b1d29b507285af27eee22e9fc2ee589266c85e2
Vasco <vhorta73@users.noreply.github.com>
2025-07-27 23:05:36 +0100

    In-10 Preparing code for Documentation (#9)
    * Adds Doxygen file.
    
    ---------
    
    Co-authored-by: Vasco <vhorta@gmail.com>

--- Interstellar/engine/include/Interstellar/Graphics/Core/IGraphics.hpp
+++ Interstellar/engine/include/Interstellar/Graphics/Core/IGraphics.hpp
@@ -25,27 +25,17 @@ namespace Interstellar::Graphics::Core {
 
         virtual bool Initialize(uint32_t width, uint32_t height, bool vsync) = 0;
         virtual void Shutdown() = 0;
-
         virtual void BeginFrame() = 0;
         virtual void EndFrame() = 0;
-
         virtual void Resize(uint32_t width, uint32_t height) = 0;
-
         virtual std::shared_ptr<IMesh> CreateMesh(const void* vertexData, size_t vertexSize,
             const void* indexData, size_t indexSize) = 0;
-
         virtual std::shared_ptr<ITexture> CreateTexture(const std::string& path) = 0;
-
         virtual std::shared_ptr<IShader> CreateShader(const std::string& path) = 0;
-
         virtual std::shared_ptr<IRenderPipeline> CreatePipeline(std::shared_ptr<IShader> shader) = 0;
-
         virtual void SubmitMesh(std::shared_ptr<IMesh> mesh, std::shared_ptr<IRenderPipeline> pipeline) = 0;
-
         virtual std::string GetRendererName() const = 0;
         virtual GraphicsAPI GetAPI() const = 0;
-
         virtual bool ShouldClose() const = 0;
     };
-
 }
