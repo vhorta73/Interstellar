763b6ea8f22cdac1f384032132f4f0831b8505d7
Vasco <vhorta@gmail.com>
2025-07-29 20:06:27 +0100

    Updates Interstellar and main.

--- Interstellar/game/src/Interstellar/Interstellar.cpp
+++ Interstellar/game/src/Interstellar/Interstellar.cpp
@@ -1,33 +1,37 @@
-#include "Interstellar/Interstellar.hpp"
 #include <iostream>
+
+#include "Interstellar/Interstellar.hpp"
+#include "Interstellar/Core/Logging.hpp"
 
 namespace Interstellar {
 
+    static const Core::Logger s_Logger(Core::LOG_INIT);
+
     Game::Game() {
-        std::cout << "Game constructed\n";
+        s_Logger.LogDebug("Game constructed.");
     }
 
     Game::~Game() {
-        std::cout << "Game destructed\n";
+        s_Logger.LogDebug("Game destructed.");
     }
 
     void Game::loadConfig() {
-        std::cout << "Loading config...\n";
+        s_Logger.LogInfo("Loading config...");
         // Load your config here.
     }
 
     void Game::buildComponents() {
-        std::cout << "Building components...\n";
+        s_Logger.LogInfo("Building components...");
         // Setup systems.
     }
 
     void Game::start() {
-        std::cout << "Game started!\n";
+        s_Logger.LogInfo("Game started!");
         // Main loop.
     }
 
     void Game::shutdown() {
-        std::cout << "Game shutting down!\n";
+        s_Logger.LogInfo("Game shutting down!");
         // Shutting down.
     }
 
