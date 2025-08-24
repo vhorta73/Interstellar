78daa9a22fc47d9921ed5f4495cb4fcde2fd188b
Vasco <vhorta@gmail.com>
2025-08-24 17:06:17 +0100

    Extends KeyCodes to other keys that were missing.

--- Input/include/Interstellar/Input/KeyCodes.hpp
+++ Input/include/Interstellar/Input/KeyCodes.hpp
@@ -6,22 +6,44 @@ namespace Interstellar::Input {
     enum class KeyCode : uint16_t {
         Unknown = 0,
 
-        // Control
+        // Control / Editing
         Escape, Enter, Tab, Backspace, Space,
+        CapsLock, NumLock, ScrollLock,
+        PrintScreen, Pause,
 
-        // Arrows
+        // Modifiers
+        LeftShift, RightShift,
+        LeftControl, RightControl,
+        LeftAlt, RightAlt,
+        LeftSuper, RightSuper, // Win/Command
+        Menu,
+
+        // Navigation
         ArrowLeft, ArrowRight, ArrowUp, ArrowDown,
+        Insert, Delete, Home, End, PageUp, PageDown,
 
-        // Digits
+        // Top number row (main keyboard)
         Num0, Num1, Num2, Num3, Num4, Num5, Num6, Num7, Num8, Num9,
+
+        // Punctuation on main rowit status
+        Minus, Equal,                // '-' and '='  (use Shift+'=' for '+')
+        LeftBracket, RightBracket,   // '[' and ']'
+        Semicolon, Apostrophe,       // ';' and '\''
+        Comma, Period, Slash,        // ',' '.' '/'
+        Backslash, GraveAccent,      // '\\' and '`'  (tilde is Shift+'`')
 
         // Letters
         A, B, C, D, E, F, G, H, I, J,
         K, L, M, N, O, P, Q, R, S, T,
         U, V, W, X, Y, Z,
 
-        // Function keys (subset)
+        // Function keys
         F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12,
+        F13, F14, F15, F16, F17, F18, F19, F20, F21, F22, F23, F24,
+
+        // Keypad
+        KP0, KP1, KP2, KP3, KP4, KP5, KP6, KP7, KP8, KP9,
+        KPDecimal, KPDivide, KPMultiply, KPSubtract, KPAdd, KPEnter, KPEqual,
 
         _Count
     };
